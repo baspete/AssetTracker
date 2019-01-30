806feeb5553d20f282a883bd8686c814d8ce0ff1
Pete Butler <pbutler@basdesign.com>
2019-01-29 18:22:51 -0800

    screw google. auth in azure is easier. going back to tablestorage

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
 /* global require console process Promise module */
 
-const express = require('express'),
-  parser = require('body-parser'),
-  admin = require('firebase-admin'),
-  Firestore = require('@google-cloud/firestore'),
+const express = require("express"),
+  azure = require("azure-storage"),
+  parser = require("body-parser"),
   app = express();
 
-const firebase_private_key = process.env.FIREBASE_PRIVATE_KEY || '';
+// Expecting AZURE_STORAGE_CONNECTION_STRING env variable to be set
+let tableSvc = azure.createTableService();
+
 // ========================================================================
 // SETUP
 
+const assets = [{ coreid: "e00fce6860329a838e34eaa" }];
+
 const params = [
   // GPS
-  { name: 'lat', type: 'string' },
-  { name: 'latitude', type: 'float' },
-  { name: 'lon', type: 'string' },
-  { name: 'longitude', type: 'float' },
-  { name: 'speed', type: 'float' },
-  { name: 'angle', type: 'int' },
-  { name: 'fixquality', type: 'int' },
+  { name: "lat", type: "string" },
+  { name: "latitude", type: "float" },
+  { name: "lon", type: "string" },
+  { name: "longitude", type: "float" },
+  { name: "speed", type: "float" },
+  { name: "angle", type: "int" },
+  { name: "fixquality", type: "int" },
   // BNO55
-  { name: 'x', type: 'int' },
-  { name: 'y', type: 'int' },
-  { name: 'z', type: 'int' },
+  { name: "x", type: "int" },
+  { name: "y", type: "int" },
+  { name: "z", type: "int" },
   // MCP9808
-  { name: 'temp1', type: 'float' },
+  { name: "temp1", type: "float" },
   // Voltages
-  { name: 'v1', type: 'float' },
-  { name: 'v2', type: 'float' }
+  { name: "v1", type: "float" },
+  { name: "v2", type: "float" }
 ];
 
 // parse application/x-www-form-urlencoded
@@ -35,92 +38,100 @@ app.use(parser.urlencoded({ extended: false }));
 // parse application/json
 app.use(parser.json());
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: 'basdesign-asset-tracker',
-    clientEmail:
-      'firebase-adminsdk-yqacy@basdesign-asset-tracker.iam.gserviceaccount.com',
-    privateKey: firebase_private_key.replace(/\\n/g, '\n')
-  }),
-  databaseURL: 'https://basdesign-asset-tracker.firebaseio.com'
-});
-
-let db = admin.firestore();
-db.settings = { timestampsInSnapshots: true };
-
 // ========================================================================
 // PRIVATE METHODS
 
 function formatFix(fix) {
-  const data = fix.data.split(',');
+  const data = fix.data.split(",");
   let newData = {};
   for (let i = 0; i < params.length; i++) {
     switch (params[i].type) {
-    case 'int':
-      newData[params[i].name] = parseInt(data[i]);
-      break;
-    case 'float':
-      newData[params[i].name] = parseFloat(data[i]);
-      break;
-    default:
-      // just send a string
-      newData[params[i].name] = data[i];
+      case "int":
+        newData[params[i].name] = parseInt(data[i]);
+        break;
+      case "float":
+        newData[params[i].name] = parseFloat(data[i]);
+        break;
+      default:
+        // just send a string
+        newData[params[i].name] = data[i];
     }
   }
-  newData['type'] = 'fix';
+  newData["type"] = "fix";
   fix.data = newData;
   return fix;
 }
 
 function saveFix(req, res) {
-  const coreid = req.body['coreid'],
-    doc = req.body['published_at'],
+  const coreid = req.body["coreid"],
+    timestamp = req.body["published_at"],
     data = formatFix(req.body).data;
 
-  console.log('Storing', JSON.stringify(data));
+  tableSvc.createTableIfNotExists(
+    coreid.toString(),
+    (error, result, response) => {
+      if (!error) {
+        // Table exists or created
+        const entity = Object.assign(
+          {
+            PartitionKey: "fix",
+            RowKey: timestamp
+          },
+          data
+        );
 
-  db.collection(coreid)
-    .doc(doc)
-    .set(data)
-    .then(response => {
-      console.log('Success!', response);
-      res.status(201).send(`Saved fix ${doc} for ${coreid}`);
-    })
-    .catch(error => {
-      console.log('SuccessError!', error);
-      res.status(400).send(error);
-    });
+        console.log("Storing", JSON.stringify(entity));
+
+        // Send it to Azure Table Storage
+        tableSvc.insertOrReplaceEntity(
+          coreid,
+          entity,
+          (error, result, response) => {
+            if (!error) {
+              res.status(201).send(response);
+            } else {
+              res.status(400).send(error);
+            }
+          }
+        );
+      } else {
+        res.status(400).send(error);
+      }
+    }
+  );
 }
 
 function createEvent(req, res) {
   if (req.body && req.body.event) {
     switch (req.body.event) {
-    case 'fix':
-      saveFix(req, res);
-      break;
-    default:
-      res.status(400).send('Failed: Unknown event type');
+      case "fix":
+        saveFix(req, res);
+        break;
+      default:
+        res.status(400).send("Failed: Unknown event type");
     }
   } else {
-    res.status(400).send('Failed: No data to process');
+    res.status(400).send("Failed: No data to process");
   }
 }
 
 // ========================================================================
 // API ENDPOINTS
 
-app.put('/api/events', (req, res) => {
+app.put("/api/events", (req, res) => {
   createEvent(req, res);
 });
+
+app.get("/api/events", (req, res) => {});
 
 // ========================================================================
 // WEB APP
 
-app.use('/', express.static('public'));
+app.use("/", express.static("public"));
 
 // ========================================================================
 // INIT
 
 const port = process.env.PORT || 9000;
 app.listen(port);
-console.log('Asset tracker app started on port ' + port);
+console.log("Asset tracker app started on port " + port);
